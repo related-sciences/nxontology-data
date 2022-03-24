ac22fb2e22ba14285b929aa051cf32d74e75e5b4
Daniel Himmelstein <dhimmel@related.vc>
2022-03-24 14:38:45 -0400

    cli & create CI workflow

---
+++ nxontology_imports/commands.py
@@ -0,0 +1,17 @@
+import logging
+
+import fire
+
+from nxontology_imports.pubchem.classfications import export_all_heirarchies
+
+
+def cli() -> None:
+    """
+    Run like `poetry run nxontology_import`
+    """
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    commands = {
+        "pubchem": export_all_heirarchies,
+    }
+    fire.Fire(commands)
